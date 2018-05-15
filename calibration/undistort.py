4ce71d500364b483451f94aea0b4435133d1c895
Jiachen Xu <jcx97119@gmail.com>
2018-05-15 14:44:23 +0800

    undistort images

--- calibration/undistort.py
+++ calibration/undistort.py
@@ -2,24 +2,34 @@ import cv2
 import calibrate
 import os
 
-# calibrate first
-rms, camera_matrix, dist_coeffs, rvecs, tvecs = calibrate.calibrate()
 
-output_path = './output/'
-for file in os.listdir(output_path):
-    distort_im = cv2.imread(output_path + '/' + file)
-    distort_im = cv2.cvtColor(distort_im, cv2.COLOR_RGB2GRAY)
+def undistort(distort_im, camera_matrix, dist_coeffs):
+        h, w = distort_im.shape[:2]
+        new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (w, h), 1, (w, h))
 
-    h, w = distort_im.shape[: 2]
-    new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, dist_coeffs, (w, h), 1, (w, h))
+        dst = cv2.undistort(distort_im, camera_matrix, dist_coeffs, None, new_camera_matrix)
 
-    dst = cv2.undistort(distort_im, camera_matrix, dist_coeffs, None, new_camera_matrix)
+        # crop and save the image
+        # without cropping the edge would be black
+        x, y, w, h = roi
+        dst = dst[y:y + h, x:x + w]
 
-    # crop and save the image
-    # without cropping the edge would be black
-    x, y, w, h = roi
-    dst = dst[y:y+h, x:x+w]
+        return dst
 
-    outfile = output_path + file[:6] + '_undistorted.jpg'
-    print('Undistorted image written to: {}'.format(outfile))
-    cv2.imwrite(outfile, dst)
+
+if __name__ == '__main__':
+    # calibrate first
+    _, camera_matrix, dist_coeffs, _, _ = calibrate.calibrate()
+
+    # used the image saved in calibrate.calibrate()
+    input_path = './output/'
+    output_path = './output/'
+    for file in os.listdir(input_path):
+        distort_im = cv2.imread(input_path + '/' + file)
+        distort_im = cv2.cvtColor(distort_im, cv2.COLOR_RGB2GRAY)
+
+        undistort_im = undistort(distort_im, camera_matrix, dist_coeffs)
+
+        outfile = output_path + file[:6] + '_undistorted.jpg'
+        print('Undistorted image written to: {}'.format(outfile))
+        cv2.imwrite(outfile, undistort_im)
